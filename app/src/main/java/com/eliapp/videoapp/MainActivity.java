d04b5e4c552d1792fc206cee4ea7d8eb95df1bd5
Carlos Eli <carloselilopezt@gmail.com>
2018-11-06 18:37:46 -0600

    Files commit

---
+++ app/src/main/java/com/eliapp/videoapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.eliapp.videoapp;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVideoView = findViewById(R.id.videoview);
+
+        MediaController controller = new MediaController( this);
+
+        controller.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controller);
+    }
+
+    private void initializePlayer(){
+        mVideoView.setVideoPath("https://www.ebookfrenzy.com/android_book/movie.mp4");
+        mVideoView.start();
+    }
+
+    private void releasePlayer(){
+        mVideoView.stopPlayback();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        initializePlayer();
+    }
+
+    protected void onStop(){
+        super.onStop();
+        releasePlayer();
+    }
+
+    protected void onPause(){
+        super.onPause();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N){
+            mVideoView.pause();
+        }
+    }
+}
